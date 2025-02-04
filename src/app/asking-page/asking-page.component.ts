aad93ee0cc0b620322af9957c2728f385fd52b72
QuentinNguyen <quentin.nguyen@edu.ece.fr>
2025-02-04 23:19:16 +0100

    Valentines project -contents

---
+++ src/app/asking-page/asking-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asking-page',
+  imports: [],
+  templateUrl: './asking-page.component.html',
+  styleUrl: './asking-page.component.css'
+})
+export class AskingPageComponent {
+
+}
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  standalone: true,
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+  gifLink = 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWlsc3R4bm53cDllZ3Nwb2owejUyb3VnamtsaDhhN3N3Y213MTBkZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26gN2pxF0yTSeZFqo/giphy.gif'
+}
